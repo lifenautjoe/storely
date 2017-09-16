27afaafdb4bd7ab6f66e81ad4c2c645f0e37f332
Joel Hernandez <involvmnt@gmail.com>
2017-09-16 16:40:34 +0200

    feat(index): export all

---
+++ lib/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @author Joel Hernandez <lifenautjoe@gmail.com>
+ */
+
+export {StorelyFactory} from './storely-factory';
+export * from './storely-interfaces';
+export * from './storely-types';
