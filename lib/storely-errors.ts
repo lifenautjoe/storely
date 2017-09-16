2df3157fc96584f6f481238e18e9ac3b5c73c78e
Joel Hernandez <involvmnt@gmail.com>
2017-09-16 16:39:47 +0200

    feat(storely-errors): create

---
+++ lib/storely-errors.ts
@@ -0,0 +1,16 @@
+/**
+ * @author Joel Hernandez <lifenautjoe@gmail.com>
+ */
+
+export class StorelyConfigurationError extends Error {
+
+}
+
+export class StorelyFactoryConfigurationError extends Error {
+
+}
+
+
+export class StorelyKeyManagerConfigurationError extends Error {
+
+}
