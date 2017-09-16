7c4fa0d0dd9126ec0d521b0973ed8b1e4a076722
Joel Hernandez <involvmnt@gmail.com>
2017-09-16 16:39:32 +0200

    feat(storely-event-dispatch-strategies): create

---
+++ lib/storely-event-dispatch-strategies.ts
@@ -0,0 +1,35 @@
+/**
+ * @author Joel Hernandez <lifenautjoe@gmail.com>
+ */
+
+import {IStorelyEventDispatchStrategy} from './storely-interfaces';
+import {EventDispatchStrategyListener, EventListenerRemover} from './storely-types';
+
+import {EventEmitter} from 'eventemitter3';
+
+export class EventEmitterDispatchStrategy implements IStorelyEventDispatchStrategy {
+    private eventEmitter;
+
+    constructor() {
+        this.eventEmitter = new EventEmitter();
+    }
+
+    emit(eventName: string, ...eventData) {
+        this.eventEmitter.emit(eventName, ...eventData);
+    }
+
+    on(eventName: string, listener: EventDispatchStrategyListener): EventListenerRemover {
+        this.eventEmitter.addListener(eventName, listener);
+        return this.makeEventListenerRemover(eventName, listener);
+    }
+
+    private makeEventListenerRemover(eventName: string, listener: EventDispatchStrategyListener): EventListenerRemover {
+        return () => {
+            this.removeEventListener(eventName, listener);
+        };
+    }
+
+    private removeEventListener(eventName: string, listener: EventDispatchStrategyListener): void {
+        this.eventEmitter.removeListener(eventName, listener);
+    }
+}
